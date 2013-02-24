98884a5270a4ddf7d9fba1ff595b4d82d96ba272
Ed Phillips <ed@cronos.net>
2013-02-23 18:04:03 -0800

    major mods

--- cookbooks/bucardo/recipes/packages.rb
+++ cookbooks/bucardo/recipes/packages.rb
@@ -4,12 +4,29 @@
 #
 
 #
+package 'postgres-8.4' do
+  action :install
+end
+package 'libpq-dev' do
+  action :install
+end
+package 'libpq5' do
+  action :install
+end
+package 'libc6' do
+  action :install
+end
+ackage 'libdbi-perl' do
+  action :install
+end
+
+
 package 'libdbd-pg-perl' do
   action :install
 end
-package 'libdbi-perl' do
-  action :install
-end
+p
+
+
 package 'postgresql-plperl-8.4' do
   action :install
 end
