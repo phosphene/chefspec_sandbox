ccaef5bb2e8abb21d06cac12213dcd23f60dbc60
Ed Phillips <ed@cronos.net>
2013-03-04 14:41:58 -0800

    mods to default

--- cookbooks/bucardo/recipes/default.rb
+++ cookbooks/bucardo/recipes/default.rb
@@ -5,24 +5,21 @@ package 'git-core' do
   action :install
 end
 
-directory "/tmp/local/" do
+directory "#{node['bucardo']['build_dir']}" do
+  user node['bucardo']['user']
   action :create
 end
 
 
-
-
 git "checkout-bucardo" do
-  repository "git://github.com/phosphene/bucardo.git"
+  user node['bucardo']['user']
+  repository  node['bucardo']['bucardo_repo']
   reference "master"
-  destination "/tmp/local/bucardo_build"
+  destination node['bucardo']['build_dir']
 end
 
-extract_path = "/tmp/local/bucardo_build"
-bucardo_bin_path = "/usr/local/bin/bucardo"
-
 bash 'build_bucardo' do
-  cwd extract_path
+  cwd node['bucardo']['build_dir']
   user 'root'
   group 'root'
 
@@ -35,10 +32,10 @@ bash 'build_bucardo' do
   not_if {File.exists?(bucardo_bin_path) }
 end
 
-user 'bucardo' do
+user "#{node['bucardo']['user']}" do
   action :create
   shell '/bin/bash'
-  home '/home/bucardo'
+  home "/home/#{node['bucardo']['user']}"
   supports :manage_home => true
 end
 
@@ -46,21 +43,18 @@ ruby_block "modify pg_conf for bucardo install" do
   block do
     require 'chef/util/file_edit'
     nc = Chef::Util::FileEdit.new("/etc/postgresql/8.4/main/pg_hba.conf")
-    nc.insert_line_after_match(/local.*?postgres.*ident/, "local   all      bucardo        trust")
+    nc.insert_line_after_match(/local.*?postgres.*ident/, "local   all      #{['bucardo']['user']}        trust")
     nc.write_file
-    Chef::Log.info "Inserted bucardo trust"
-    not_if 'psql --list|grep bucardo', :user => 'postgres'
+    Chef::Log.info "Inserted #{node['bucardo']['user']} trust"
+    not_if "psql --list|grep #{node['bucardo']['user']}", :user => 'postgres'
   end
 end
 
 
 service 'postgresql-8.4' do
   action :restart
-  not_if 'psql --list|grep bucardo', :user => 'postgres'
+  not_if "psql --list|grep #{node['bucardo']['user']}", :user => 'postgres'
 end
-
-
-
 
 
 bash 'install_bucardo' do
@@ -70,5 +64,6 @@ bash 'install_bucardo' do
     bucardo install --batch
     EOH
   action :run
-  not_if 'psql --list|grep bucardo', :user => 'postgres'
+  not_if "psql --list|grep #{node['bucardo']['user']}", :user => 'postgres'
+
 end
