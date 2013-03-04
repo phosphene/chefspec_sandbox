405be097cd90e2e2975d9510e95609fcd46f2565
Ed Phillips <ed@cronos.net>
2013-03-04 14:41:33 -0800

    fix attributes

--- cookbooks/bucardo/attributes/default.rb
+++ cookbooks/bucardo/attributes/default.rb
@@ -2,7 +2,7 @@
 ##install
 default['bucardo']['build_dir'] = '/tmp/local/bucardo_build'
 default['bucardo']['user'] = 'bucardo'
-default['bucardo']['bucardo_repo'] = 'http://www.github.com/bucardo/bucardo.git'
+default['bucardo']['bucardo_repo'] = 'git://github.com/bucardo/bucardo.git'
 default['bucardo']['bin_path'] = "/usr/local/bin/bucardo"
 
 ##configure
