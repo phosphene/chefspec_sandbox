530c00a1f126af120bdccca6bc965600d780ee98
phosphene <ed@cronos.net>
2013-03-11 19:54:45 -0400

    mods to util_lib

--- cookbooks/bucardo/libraries/util_lib.rb
+++ cookbooks/bucardo/libraries/util_lib.rb
@@ -1,14 +1,10 @@
 module UtilLib
 
-
-  def get_remote_db_backup (config) 
+  def get_remote_db_backup (config)
     bucket = get_storage_bucket config
     bucket.files.all(:prefix => "#{config['client']}/").last
   end
 
- def test_this_method 
-    puts "I am a test"
-  end
 
   def get_storage_bucket(config)
 
@@ -17,7 +13,7 @@ module UtilLib
                                :aws_access_key_id => config['aws_access_key_id'],
                                :aws_secret_access_key => config['aws_secret_access_key']
                                )
-    storage.directories.get('color-db-backup')    
+    storage.directories.get('color-db-backup')
   end
 
 
