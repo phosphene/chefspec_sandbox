1ad662682a7c333df222b8e32d0b4ffcf2e145bf
phosphene <ed@cronos.net>
2013-03-15 12:05:46 -0400

    mods

--- cookbooks/bucardo/recipes/setup_slave.rb
+++ cookbooks/bucardo/recipes/setup_slave.rb
@@ -1,7 +1,29 @@
+#get my secrets
 bucardo_secret = Chef::EncryptedDataBagItem.load_secret("#{node['bucardo']['secretpath']}")
 bucardo_creds = Chef::EncryptedDataBagItem.load("passwords", "bucardo", bucardo_secret)
 
+
 #namespace to local
+
+#dbname and master and slave dbs
+dbname = node.bucardo.dbname
+master = node.bucardo.master
+slave = node.bucardo.slave
+master_pass = bucardo_creds["#{master['user']}"]
+slave_pass = bucardo_creds["#{slave['user']}"]
+
+#bucardo groups and syncs
+pushdelta_relgroup = node.bucardo.pushdelta_relgroup
+fullcopy_relgroup = node.bucardo.fullcopy_relgroup
+fullcopy_dbgroup = node.bucardo.fullcopy_dbgroup
+fullcopy_sync = node.bucardo.fullcopy_sync
+pushdelta_sync = node.bucardo.pushdelta_sync
+
+#exclude and include tables
+excluded_delta_tables_array = node.bucardo.excluded_delta_tables_array
+include_fullcopy_tables_array = node.bucardo.include_fullcopy_tables_array
+
+#namespace to local for shorter, more succint lines
 dbname = node.bucardo.dbname
 master = node.bucardo.master
 slave = node.bucardo.slave
@@ -19,7 +41,7 @@ pushdelta_sync = node.bucardo.pushdelta_sync_name
 excluded_delta_tables_array = node.bucardo.excluded_delta_tables_array
 include_fullcopy_tables_array = node.bucardo.include_fullcopy_tables_array
 
-
+#begin recipe
 
 execute "alter_bucardo_password" do
   user 'postgres'
@@ -43,7 +65,6 @@ execute 'append to pgpass file' do
 end
 
 
-
 file '/var/lib/postgresql/.pgpass' do
   owner 'postgres'
   mode "600"
@@ -63,7 +84,6 @@ bash "dump master schema and load to local slave" do
 end
 
 
-
 execute  'add master db to bucardo' do
   user 'bucardo'
   command %| bucardo add db #{dbname}_master dbname=#{dbname} host=#{master['host']} dbuser=#{master['user']}  pass=#{master_pass} |
@@ -76,6 +96,103 @@ execute  'add slave db to bucardo' do
   command %|bucardo add db #{dbname}_slave dbname=#{dbname} host=#{slave['host']} dbuser=#{slave['user']} pass=#{slave_pass} |
   action :run
 end
+
+
+
+
+execute  'add tables to bucardo pushdelta relgroup' do
+  user 'bucardo'
+  command %{bucardo add table all relgroup=#{pushdelta_relgroup} db=#{dbname}_master}
+  action :run
+end
+
+excluded_delta_tables_array.each do |val|
+
+  execute "remove excluded #{val} table with no primary key" do
+    user 'bucardo'
+    command %|bucardo remove table #{val} |
+    action :run
+  end
+
+end
+
+
+
+execute  'add sequences to bucardo pushdelta relgroup' do
+  user 'bucardo'
+  command %|bucardo add sequence all relgroup=#{pushdelta_relgroup} db=#{dbname}_master |
+  action :run
+end
+
+execute  'create pushdelta dbgroup' do
+  user 'bucardo'
+  command %|bucardo add dbgroup #{pushdelta_dbgroup} #{dbname}_slave:target #{dbname}_master:source|
+    action :run
+end
+
+
+execute  'create pushdelta sync' do
+  user 'bucardo'
+  command %| bucardo add sync #{pushdelta_sync} relgroup=#{pushdelta_relgroup} dbs=#{pushdelta_dbgroup} onetimecopy=2|
+    action :run
+end
+
+
+execute  'create dbgroup for full_copy' do
+  user 'bucardo'
+  command %|bucardo add dbgroup #{fullcopy_dbgroup} #{dbname}_master:source  #{dbname}_slave:fullcopy|
+  action :run
+end
+
+execute  'create relgroup for full_copy' do
+  user 'bucardo'
+  command %|bucardo add relgroup #{fullcopy_relgroup} db=#{dbname}_master |
+  action :run
+end
+
+include_fullcopy_tables_array.each do |val|
+  execute "add full_copy of #{val} table with no primary key" do
+    user 'bucardo'
+    command %|bucardo add table #{val} relgroup=#{fullcopy_relgroup} db=#{dbname}_master|
+    action :run
+  end
+end
+
+
+execute  'create fullcopy sync' do
+  user 'bucardo'
+  command %| bucardo add sync #{fullcopy_sync} relgroup=#{fullcopy_relgroup} dbs=#{fullcopy_dbgroup} onetimecopy=2|
+  action :run
+end
+
+
+directory '/var/run/bucardo' do
+  user 'bucardo'
+  group 'bucardo'
+  action :create
+end
+
+
+directory '/var/log/bucardo' do
+  user 'bucardo'
+  group 'bucardo'
+  action :create
+end
+
+
+execute  'start bucardo' do
+  cwd '/var/run/bucardo'
+  user 'bucardo'
+  command 'bucardo start'
+  action :run
+end
+
+
+
+
+
+
+
 
 # execute  'add tables to bucardo relgroup' do
 #   user 'bucardo'
--- cookbooks/bucardo/test/kitchen/cookbooks/bucardo_test/recipes/post_setup_slave_test.rb
+++ cookbooks/bucardo/test/kitchen/cookbooks/bucardo_test/recipes/post_setup_slave_test.rb
@@ -29,100 +29,100 @@ include_fullcopy_tables_array = node.bucardo.include_fullcopy_tables_array
 
 
 
-execute  'add tables to bucardo pusdelta relgroup' do
-  user 'bucardo'
-  command %{bucardo add table all relgroup=#{pushdelta_relgroup} db=#{dbname}_master}
-  action :run
-end
+# execute  'add tables to bucardo pushdelta relgroup' do
+#   user 'bucardo'
+#   command %{bucardo add table all relgroup=#{pushdelta_relgroup} db=#{dbname}_master}
+#   action :run
+# end
 
-excluded_delta_tables_array.each do |val|
+# excluded_delta_tables_array.each do |val|
 
-  execute "remove excluded #{val} table with no primary key" do
-    user 'bucardo'
-    command %|bucardo remove table #{val} |
-    action :run
-  end
+#   execute "remove excluded #{val} table with no primary key" do
+#     user 'bucardo'
+#     command %|bucardo remove table #{val} |
+#     action :run
+#   end
 
-end
-
-
-
-execute  'add sequences to bucardo pushdelta relgroup' do
-  user 'bucardo'
-  command %|bucardo add sequence all relgroup=#{pushdelta_relgroup} db=#{dbname}_master |
-  action :run
-end
-
-execute  'create pushdelta dbgroup' do
-  user 'bucardo'
-  command %|bucardo add dbgroup #{pushdelta_dbgroup} #{dbname}_slave:target #{dbname}_master:source|
-    action :run
-end
-
-
-execute  'create pushdelta sync' do
-  user 'bucardo'
-  command %| bucardo add sync #{pushdelta_sync} relgroup=#{pushdelta_relgroup} dbs=#{pushdelta_dbgroup} onetimecopy=2|
-    action :run
-end
-
-
-execute  'create dbgroup for full_copy' do
-  user 'bucardo'
-  command %|bucardo add dbgroup #{fullcopy_dbgroup} #{dbname}_master:source  #{dbname}_slave:fullcopy|
-  action :run
-end
-
-execute  'create relgroup for full_copy' do
-  user 'bucardo'
-  command %|bucardo add relgroup #{fullcopy_relgroup} db=#{dbname}_master |
-  action :run
-end
-
-include_fullcopy_tables_array.each do |val|
-  execute "add full_copy of #{val} table with no primary key" do
-    user 'bucardo'
-    command %|bucardo add table #{val} relgroup=#{fullcopy_relgroup} db=#{dbname}_master|
-    action :run
-  end
-end
+# end
 
 
 
+# execute  'add sequences to bucardo pushdelta relgroup' do
+#   user 'bucardo'
+#   command %|bucardo add sequence all relgroup=#{pushdelta_relgroup} db=#{dbname}_master |
+#   action :run
+# end
 
-execute  'create fullcopy sync' do
-  user 'bucardo'
-  command %| bucardo add sync #{fullcopy_sync} relgroup=#{fullcopy_relgroup} dbs=#{fullcopy_dbgroup} onetimecopy=1|
-  action :run
-end
-
-
-directory '/var/run/bucardo' do
-  user 'bucardo'
-  group 'bucardo'
-  action :create
-end
+# execute  'create pushdelta dbgroup' do
+#   user 'bucardo'
+#   command %|bucardo add dbgroup #{pushdelta_dbgroup} #{dbname}_slave:target #{dbname}_master:source|
+#     action :run
+# end
 
 
-directory '/var/log/bucardo' do
-  user 'bucardo'
-  group 'bucardo'
-  action :create
-end
+# execute  'create pushdelta sync' do
+#   user 'bucardo'
+#   command %| bucardo add sync #{pushdelta_sync} relgroup=#{pushdelta_relgroup} dbs=#{pushdelta_dbgroup} onetimecopy=2|
+#     action :run
+# end
 
 
-execute  'start bucardo' do
-  cwd '/var/run/bucardo'
-  user 'bucardo'
-  command 'bucardo start'
-  action :run
-end
+# execute  'create dbgroup for full_copy' do
+#   user 'bucardo'
+#   command %|bucardo add dbgroup #{fullcopy_dbgroup} #{dbname}_master:source  #{dbname}_slave:fullcopy|
+#   action :run
+# end
 
-execute  'activate sync' do
-  user 'bucardo'
-  command %| bucardo activate sync #{pushdelta_sync}|
-  action :run
-end
+# execute  'create relgroup for full_copy' do
+#   user 'bucardo'
+#   command %|bucardo add relgroup #{fullcopy_relgroup} db=#{dbname}_master |
+#   action :run
+# end
+
+# include_fullcopy_tables_array.each do |val|
+#   execute "add full_copy of #{val} table with no primary key" do
+#     user 'bucardo'
+#     command %|bucardo add table #{val} relgroup=#{fullcopy_relgroup} db=#{dbname}_master|
+#     action :run
+#   end
+# end
+
+
+
+
+# execute  'create fullcopy sync' do
+#   user 'bucardo'
+#   command %| bucardo add sync #{fullcopy_sync} relgroup=#{fullcopy_relgroup} dbs=#{fullcopy_dbgroup} onetimecopy=1|
+#   action :run
+# end
+
+
+# directory '/var/run/bucardo' do
+#   user 'bucardo'
+#   group 'bucardo'
+#   action :create
+# end
+
+
+# directory '/var/log/bucardo' do
+#   user 'bucardo'
+#   group 'bucardo'
+#   action :create
+# end
+
+
+# execute  'start bucardo' do
+#   cwd '/var/run/bucardo'
+#   user 'bucardo'
+#   command 'bucardo start'
+#   action :run
+# end
+
+# execute  'activate sync' do
+#   user 'bucardo'
+#   command %| bucardo activate sync #{pushdelta_sync}|
+#   action :run
+# end
 
 
 
@@ -133,11 +133,11 @@ execute  'kick fullcopy sync' do
 end
 
 
-execute  'activate sync' do
-  user 'bucardo'
-  command %| bucardo activate sync #{fullcopy_sync}|
-  action :run
-end
+# execute  'activate sync' do
+#   user 'bucardo'
+#   command %| bucardo activate sync #{fullcopy_sync}|
+#   action :run
+# end
 
 
 execute  'kick fullcopy sync' do
